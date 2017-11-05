cb9cff48b2dde4e3e8bae8ef878540d908f93d48
gdelrio87 <gdelrioala@gmail.com>
2017-11-05 11:37:51 +0100

    EPD5-P2

--- epd4/p2/index.php
+++ epd4/p2/index.php
@@ -2,23 +2,15 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" type="text/css" href="style.css" />
-        <title>Epd_4_p2</title>
+        <link rel="stylesheet" type="text/css" href="estilo.css" />
+        <title></title>
     </head>
     <body>
-        <p>Mejore el sistema de detección de frases cortas/largas desarrollado en el ejercicio anterior de tal forma que solvente
-            automáticamente las frases que exceden los límites. Para ello, por una parte, dispondremos de un vector de palabras que servirá
-            para rellenar texto en las frases cortas. En concreto, las palabras del vector se irán intercalando entre palabra y palabra en las
-            frases cortas hasta que éstas alcancen el umbral mínimo. Para las frases largas, se dispondrá de un vector asociativo de
-            abreviaturas (palabra → abreviatura), de manera que habrá que buscar en las frases largas las palabras que estén en el vector de
-            abreviaturas y sustituirlas por éstas. Si, tras esta sustitución, la frase sigue sobrepasando el límite, se eliminarán todos los
-            caracteres que sobren (desde el final de la frase) e introducir puntos suspensivos (...) al final de la frase. El sistema deberá mostrar
-            el texto original y el texto al que se le han aplicado las correcciones automáticas, marcando éstas con un formato de énfasis
-            (distinto al empleado en el ejercicio anterior).</p>
-        <form method="post" action="recogidaDatos.php">
+        <form method="post" action="phpRes.php">
             Introduzca el Texto: <br>
-            <textarea name="textarea" rows="6" cols="60" required="on" maxlength="10000" minlength="1" >hola.Dos palabras.cinco ya es demasiado.</textarea>
-            <br><br> 
+            <textarea name="textarea" rows="6" cols="60" required="on" maxlength="1000" minlength="1" >juan. 
+miarma y como que. hola pepe como estas hijo. yo bien y tu. que tal estas, espero que muy bien besos.</textarea>
+            <br><br>
             Minimo: <input type="range" name="des1" min="1" max="10">
             <br><br>
             Máximo: <input type="range" name="des2" min="1" max="10">
@@ -28,8 +20,3 @@
         </form>
     </body>
 </html>
-<!--Amplíe la página desarrollada en el problema 2 del guión de la EPD anterior de tal forma que permita considerar
-textos dirigidos a un servicio de soporte técnico de redes. En concreto, el texto deberá contener una dirección IP origen: 142.40.14.32, una
-dirección IP destino: 127.0.0.1  y dos números de puertos TCP en el rango 1000-1500. Para ello, verifique que en el texto aparecen al menos
-dos direcciones IP y dos números comprendidos en el intervalo [1000, 1500]. Use la función filter_var() de PHP, o bien expresiones-->
-<!--regulares, para detectar este tipo de contenidos en el texto.-->
---
+++ epd4/p2/phpRes.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="estilo.css" />
+        <title></title>
+    </head>
+    <?PHP
+
+    function autocompletar($sep,$vector,$contPAlabras,$i) {
+        
+        $pos = 0;
+        $sepArray = explode(" ", $sep[$i]);
+        while ($contPAlabras < $_POST["des1"]) {
+            $finalArray = array();
+
+            for ($cont = 0; $cont < count($sepArray); $cont++) {
+
+                array_push($finalArray, $sepArray[$cont]);
+                $final = false;
+                if ($cont <= $pos) {
+                    array_push($finalArray, $vector[$cont]);
+                    $final = true;
+                    $contPAlabras++;
+                }
+            }
+            $pos++;
+            if ($final === true) {
+                while ($contPAlabras < $_POST["des1"]) {
+                    
+                    array_push($finalArray, $vector[$pos]);
+                    $contPAlabras++;
+                    $pos++;
+                    if($pos==count($vector)){
+                        $pos = 0;
+                    }
+                }
+            }
+        }
+        return $finalArray;
+    }
+    ?>
+    <body>
+        <?php
+        if (isset($_POST['submit'])) {
+            $pos = 0;
+            $contPAlabras = 0;
+            $encontrado = true;
+            $vector = ["autoCompl1", "autoCompl2", "autoCompl3", "autoCompl4"];
+            $diccionario = array(
+    "espero"  => "xro",
+    "bien"  => ":)",
+    "besos"  => "muak",
+    "que"  => "q",
+);
+
+            $sep = explode('.', $_POST["textarea"]);
+
+            $unir = "";
+            if ($_POST["des1"] < $_POST["des2"]) {
+                for ($i = 0; $i < count($sep) - 1; $i++) {
+                    $contPAlabras = str_word_count($sep[$i]);
+
+                    if (($contPAlabras >= $_POST["des1"]) && ($contPAlabras <= $_POST["des2"])) {
+                        $unir .= "$sep[$i].";
+                    } else if ($contPAlabras < $_POST["des1"]) {
+                        $finalArray = autocompletar($sep,$vector,$contPAlabras,$i);
+                        $encontrado = false;
+
+                        $unir .= "<em><b>" . implode(" ", $finalArray) . "</b></em>.";
+                    } else if ($contPAlabras > $_POST["des2"]) {
+                       
+                        $encontrado = false;
+                        $sepArray = explode(" ", $sep[$i]);
+                        
+                        for($m=0;$m<count($sepArray);$m++){
+                            
+                            if(array_key_exists($sepArray[$m],$diccionario)){
+                                
+                                $sepArray[$m] = $diccionario[$sepArray[$m]];
+                                
+                            }
+                        }
+                        
+                        while($contPAlabras > $_POST["des2"]){
+                            
+                            unset($sepArray[count($sepArray)-1]);
+                            $contPAlabras--;
+                        }
+                        array_push($sepArray,"...");
+                        $unir .= "<strong id='grande'>" . implode(" ", $sepArray) . "</strong>.";
+                        echo $unir;
+                    }
+                }
+            } else {
+                echo "error en el deslizador<br>";
+            }
+
+            if ($encontrado == true) {
+                echo "<h3>El texto es correcto</h3>";
+            } else {
+                echo "<h3>El texto contiene errores</h3>";
+            }
+        }
+        ?>
+    </body>
+</html>
