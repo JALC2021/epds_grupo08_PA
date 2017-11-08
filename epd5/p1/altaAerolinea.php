47fe53dd0ef942b8bc66a3e746a73e4356340cfd
jalc <jantonio2511@gmail.com>
2017-11-09 00:00:08 +0100

    epd5_p1
    falta validar un par de cosas, mostrar las ciudades con el radio buton y la ultima parte del 2 parrafo

--- epd5/p1/altaAerolinea.php
+++ epd5/p1/altaAerolinea.php
@@ -13,13 +13,12 @@ and open the template in the editor.
         <?php
         $nombreAerolinea = $_POST['nombreAerolinea'];
         $nDestinos = $_POST = ['nDestinos'];
-        $f_id_nombreAerolinea = fopen("id_nombreAerolinea.txt", 'a');
-        $LecF_id_nombreAerolinea = fopen("id_nombreAerolinea.txt", 'r');
+        $escritura_txt_id_nombreAerolinea = fopen("id_nombreAerolinea.txt", 'a');
+        $lectura_txt_id_nombreAerolinea = fopen("id_nombreAerolinea.txt", 'r');
+        $lectura_txt_ciudades = fopen("ciudades.txt", 'r');
         $id_aerolinea = NULL;
         $vectorIds = array();
-        ?>
-
-        <?php
+//
         if (!isset($_POST['siguiente'])) {
             if (!isset($nombreAerolinea)) {
                 $errores[] = 'Debe indicar el nombre de la erol&iacute;neaianea';
@@ -30,46 +29,49 @@ and open the template in the editor.
 
                     $id_aerolinea = 0;
 
-                    flock($f_id_nombreAerolinea, LOCK_EX);  //bloqueo escritura
-                    fwrite($f_id_nombreAerolinea, $id_aerolinea . ";" . $nombreAerolinea . "\n");
-                    flock($f_id_nombreAerolinea, LOCK_UN);
-                    fclose($f_id_nombreAerolinea);
+                    flock($escritura_txt_id_nombreAerolinea, LOCK_EX);  //bloqueo escritura
+                    fwrite($escritura_txt_id_nombreAerolinea, $id_aerolinea . ";" . $nombreAerolinea . "\n");
+                    flock($escritura_txt_id_nombreAerolinea, LOCK_UN);
+                    fclose($escritura_txt_id_nombreAerolinea);
                 } else {
                     //lectura del fichero para comprobar id
 //                  
-                    flock($LecF_id_nombreAerolinea, LOCK_SH);  //bloqueo lectura
-                    $leerIdAero = fgetcsv($LecF_id_nombreAerolinea, 999, ";");   //lee la primera linea
+                    flock($lectura_txt_id_nombreAerolinea, LOCK_SH);  //bloqueo lectura
+                    $leerIdAero = fgetcsv($lectura_txt_id_nombreAerolinea, 999, ";");   //lee la primera linea
 
-                    while (!feof($LecF_id_nombreAerolinea)) {
-                        $leerIdAero = fgetcsv($LecF_id_nombreAerolinea, 999, ";");
+                    while (!feof($lectura_txt_id_nombreAerolinea)) {
+                        $leerIdAero = fgetcsv($lectura_txt_id_nombreAerolinea, 999, ";");
                         $vectorIds[] = $leerIdAero[0];
                     }
 
                     $maxIds = max($vectorIds);
                     $id_aerolinea = $maxIds + 1;
 
-                    flock($LecF_id_nombreAerolinea, LOCK_UN);
-                    fclose($LecF_id_nombreAerolinea);
+                    flock($lectura_txt_id_nombreAerolinea, LOCK_UN);
+                    fclose($lectura_txt_id_nombreAerolinea);
 
-                    flock($f_id_nombreAerolinea, LOCK_EX);
-                    fwrite($f_id_nombreAerolinea, $id_aerolinea . ";" . $nombreAerolinea . "\n");
-                    flock($f_id_nombreAerolinea, LOCK_UN);
-                    fclose($f_id_nombreAerolinea);
+                    flock($escritura_txt_id_nombreAerolinea, LOCK_EX);
+                    fwrite($escritura_txt_id_nombreAerolinea, $id_aerolinea . ";" . $nombreAerolinea . "\n");
+                    flock($escritura_txt_id_nombreAerolinea, LOCK_UN);
+                    fclose($escritura_txt_id_nombreAerolinea);
                 }
             }
         }
         ?>
-
         <h2>Seleccione Destino</h2>
+
         <form method="post" action ="altaCompleta.php" name="alta">
-            <select multiple size="8" name="vectorCiudadesDestino[]">
+            <!-- si nDestinos = 3 por ejemplo debe mostrar 3 selec y selecionar uno de cada uno sin que se repita la ciudad para eso debemos eliminar el atributo multiple-->
+            <select multiple size="8" name="vectorCiudadesDestino[]">   
+            <!--<select  size="8" name="vectorCiudadesDestino[]">-->    
                 <?php
-                $f = fopen("ciudades.txt", 'r');
-                if ($f) {
-                    flock($f, LOCK_SH);
-                    $ciudades = fgetcsv($f, 999, ",");
-                    while (!feof($f)) {
-                        $ciudades = fgetcsv($f, 999, ",");
+//                $lectura_txt_ciudades = fopen("ciudades.txt", 'r');
+
+                if ($lectura_txt_ciudades) {
+                    flock($lectura_txt_ciudades, LOCK_SH);
+                    $ciudades = fgetcsv($lectura_txt_ciudades, 999, ",");
+                    while (!feof($lectura_txt_ciudades)) {
+                        $ciudades = fgetcsv($lectura_txt_ciudades, 999, ",");
                         ?>
                         <option value="Roma"><?php echo $ciudades[0] ?></option>
                         <option value="Paris"><?php echo $ciudades[1] ?></option>
@@ -82,14 +84,17 @@ and open the template in the editor.
                         <?php
                     }
 
-                    flock($f, LOCK_UN);
-                    fclose($f);
+                    flock($lectura_txt_ciudades, LOCK_UN);
+                    fclose($lectura_txt_ciudades);
                 } else {
                     echo "error en el fichero";
                 }
                 ?>
+
             </select>
-            <input type="hidden" name="id_aerolinea" value="<?php echo $_POST['id_aerolinea']; ?>">
+
+            <input type="hidden" name="id_aerolinea" value="<?php echo $id_aerolinea; ?>">
+            <input type="hidden" name="nombreAerolinea" value="<?php echo $_POST['nombreAerolinea']; ?>">
             <input type="submit" name="enviarDestino" value="Enviar">
         </form>
     </body>
--- epd5/p1/index.php
+++ epd5/p1/index.php
@@ -10,33 +10,35 @@ and open the template in the editor.
         <title>Epd_5_p1</title>
     </head>
     <body>
-        <p>Cree un sistema, compuesto por varias páginas PHP, que permita gestionar vuelos y destinos de aerolíneas.
-            La primera página del sistema permitirá dar de alta una aerolínea y sus destinos. En ella se pedirá al usuario el nombre de la
-            aerolínea y el número de destinos en los que opera. Al enviar esta información, se proporcionará al usuario un nuevo formulario en
-            el que se podrán indicar, mediante listas desplegables (campos select), las ciudades de cada uno de los destinos. Considere para
-            ello un vector con el siguiente repertorio de ciudades posibles: Roma, Paris, Londres, Dublin, Sevilla, Madrid, Barcelona y
-            Amsterdam. Este repertorio de ciudades deberá ser leído de un archivo de texto que contendrá dichas ciudades separadas por
-            comas (,). Recogidos todos estos datos, se escribirán en dos archivos. El primero almacenará dos campos: un código autonumérico
-            y el nombre de cada aerolínea. El código autonumérico deberá ser generado a partir del siguiente número al máximo de los
-            almacenados en el fichero. El segundo almacenará los nombres de los destinos de cada aerolínea (en forma de pares compuestos
-            por el código numérico de la aerolínea y el nombre del destino). Estos ficheros emplearán el formato CSV para representar los
-            datos, usando un punto y coma (;) como separador entre campos.
-        <p>La segunda página del sistema permitirá introducir los vuelos. Se mostrará un listado con las diferentes aerolíneas registradas en el
-            sistema. Justo debajo de cada nombre de aerolínea, en la misma página, se mostrará una lista de botones de radio indicando los
-            diferentes destinos en los que opera la aerolínea, para que el usuario seleccione la ciudad de origen del vuelo. El usuario
-            seleccionará uno y pulsará el botón enviar. A continuación, se le mostrará al usuario una lista displegable con aquellos destinos
-            para los que no se tiene ningún vuelo que lo conecte desde la ciudad origen seleccionada en el paso anterior (estos datos los
-            podremos obtener del fichero que se describe a continuación), así como un cuadro de texto de tipo time para especificar la duración
-            del vuelo (en horas y minutos). En caso de que todos los destinos de la aerolínea hayan sido ya conectados con el destino
-            seleccionado en el primer paso, se mostrará un mensaje de error indicando tal eventualidad. Recogidos los datos, se guardarán en
-            un archivo CSV de vuelos, donde se indique el código numérico de la aerolínea, el nombre del destino (ciudad) origen del vuelo, el
-            nombre del destino final, así como las horas y minutos de duración del vuelo.</p>
-        <p>Finalmente, una tercera página nos permitirá mostrar un informe de resumen de los destinos de una de las aerolíneas registradas
-            en el sistema. Para ello, se ofrecerá una lista con botones de radio de cada una de las aerolíneas y, al enviar esta información, se
-            ofrecerá el informe resumen indicando el nombre del destino, el número de vuelos (conexiones) que parten de él y el tiempo medio
-            de vuelo hacia otros destinos. Esta tabla mostrará los destinos ordenados de forma descendente en función del número de
-            conexiones.</p>
-        <p>Cree una página principal que permita el acceso a cada una de las páginas descritas anteriormente.</p>
+        <!--
+         <p>Cree un sistema, compuesto por varias páginas PHP, que permita gestionar vuelos y destinos de aerolíneas.
+             La primera página del sistema permitirá dar de alta una aerolínea y sus destinos. En ella se pedirá al usuario el nombre de la
+             aerolínea y el número de destinos en los que opera. Al enviar esta información, se proporcionará al usuario un nuevo formulario en
+             el que se podrán indicar, mediante listas desplegables (campos select), las ciudades de cada uno de los destinos. Considere para
+             ello un vector con el siguiente repertorio de ciudades posibles: Roma, Paris, Londres, Dublin, Sevilla, Madrid, Barcelona y
+             Amsterdam. Este repertorio de ciudades deberá ser leído de un archivo de texto que contendrá dichas ciudades separadas por
+             comas (,). Recogidos todos estos datos, se escribirán en dos archivos. El primero almacenará dos campos: un código autonumérico
+             y el nombre de cada aerolínea. El código autonumérico deberá ser generado a partir del siguiente número al máximo de los
+             almacenados en el fichero. El segundo almacenará los nombres de los destinos de cada aerolínea (en forma de pares compuestos
+             por el código numérico de la aerolínea y el nombre del destino). Estos ficheros emplearán el formato CSV para representar los
+             datos, usando un punto y coma (;) como separador entre campos.
+         <p>La segunda página del sistema permitirá introducir los vuelos. Se mostrará un listado con las diferentes aerolíneas registradas en el
+             sistema. Justo debajo de cada nombre de aerolínea, en la misma página, se mostrará una lista de botones de radio indicando los
+             diferentes destinos en los que opera la aerolínea, para que el usuario seleccione la ciudad de origen del vuelo. El usuario
+             seleccionará uno y pulsará el botón enviar. A continuación, se le mostrará al usuario una lista displegable con aquellos destinos
+             para los que no se tiene ningún vuelo que lo conecte desde la ciudad origen seleccionada en el paso anterior (estos datos los
+             podremos obtener del fichero que se describe a continuación), así como un cuadro de texto de tipo time para especificar la duración
+             del vuelo (en horas y minutos). En caso de que todos los destinos de la aerolínea hayan sido ya conectados con el destino
+             seleccionado en el primer paso, se mostrará un mensaje de error indicando tal eventualidad. Recogidos los datos, se guardarán en
+             un archivo CSV de vuelos, donde se indique el código numérico de la aerolínea, el nombre del destino (ciudad) origen del vuelo, el
+             nombre del destino final, así como las horas y minutos de duración del vuelo.</p>
+         <p>Finalmente, una tercera página nos permitirá mostrar un informe de resumen de los destinos de una de las aerolíneas registradas
+             en el sistema. Para ello, se ofrecerá una lista con botones de radio de cada una de las aerolíneas y, al enviar esta información, se
+             ofrecerá el informe resumen indicando el nombre del destino, el número de vuelos (conexiones) que parten de él y el tiempo medio
+             de vuelo hacia otros destinos. Esta tabla mostrará los destinos ordenados de forma descendente en función del número de
+             conexiones.</p>
+         <p>Cree una página principal que permita el acceso a cada una de las páginas descritas anteriormente.</p>
+        -->
         <h2>Alta Aerol&iacute;nea</h2>
         <form method="post" action =altaAerolinea.php name="alta">
             Nombre: <input type="text" name="nombreAerolinea"><br />
