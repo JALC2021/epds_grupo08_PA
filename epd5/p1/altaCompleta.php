cad04b90a16c46266081b8a1a28430b232429586
jalc <jantonio2511@gmail.com>
2017-11-14 21:45:34 +0100

    Revert "epd5_p1"
    This reverts commit 47fe53dd0ef942b8bc66a3e746a73e4356340cfd.

--- epd5/p1/altaCompleta.php
+++ epd5/p1/altaCompleta.php
@@ -10,67 +10,23 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h2>ALTA AEROLINEA: OK</h2>
         <?php
-        $vectorCiudadesDestino = $_POST['vectorCiudadesDestino'];
+        $vecCiudDest[] = $_POST['vectorCiudadesDestino'];
+        //$nombreAerolinea = $_POST['nombreAerolinea'];
         $id_aerolinea = $_POST['id_aerolinea'];
-        $nombreAerolinea = $_POST['nombreAerolinea'];
-        $escritura_txt_altaCompleta = fopen("altaCompleta.txt", 'a');    //modo escritura
-        $lectura_txt_altaCompleta = fopen("altaCompleta.txt", 'r');   //modo lectura
-        $lectura_txt_id_nombreAerolinea = fopen("id_nombreAerolinea.txt", 'r');    //modo lectura
-
-        flock($escritura_txt_altaCompleta, LOCK_EX);  //bloqueo escritura
-
-        for ($c = 0; $c < count($vectorCiudadesDestino); $c++) {
-
-            fwrite($escritura_txt_altaCompleta, $id_aerolinea . ";" . $vectorCiudadesDestino[$c] . "\n");
-        }
-        flock($escritura_txt_altaCompleta, LOCK_UN);
-        fclose($escritura_txt_altaCompleta);
+        $f_id_nombreAerolinea_Dest = fopen("altaCompleta.txt", 'a');
         ?>
-        <h2>Aerol&iacute;neas registradas</h2>
-
-
         <?php
-        //Leemos las aerolineas
-        $nombreAero = array();
-        flock($lectura_txt_id_nombreAerolinea, LOCK_SH);  //bloqueo lectura
-        $leerNomAero = fgetcsv($lectura_txt_id_nombreAerolinea, 999, ";");   //lee la primera linea
-        while (!feof($lectura_txt_id_nombreAerolinea)) {
-            $leerNomAero = fgetcsv($lectura_txt_id_nombreAerolinea, 999, ";");
+        echo $id_aerolinea;
+        flock($f_id_nombreAerolinea_Dest, LOCK_EX);
+//                    fwrite($f_id_nombreAerolinea, $id_aerolinea . ";" . $nombreAerolinea . ";" . "\n");
+//        fwrite($f_id_nombreAerolinea_Dest, $id_aerolinea . ";" . $nombreAerolinea . ";" . $vecCiudDest[] . "\n");
+        foreach ($vecCiudDest as $ciudad) {
 
-            echo $nombreAero[] = $leerNomAero[0] . "<->" . $leerNomAero[1] . "<br />";
+            fputcsv($f_id_nombreAerolinea_Dest, $id_aerolinea, $ciudad, ";");
         }
-        //leemos las ciudades destino para cada aerolinea
-        $nombreDest = array();
-        flock($lectura_txt_altaCompleta, LOCK_SH);  //bloqueo lectura
-        $leerDest = fgetcsv($lectura_txt_altaCompleta, 999, ";");   //lee la primera linea
-        while (!feof($lectura_txt_altaCompleta)) {
-            $leerDest = fgetcsv($lectura_txt_altaCompleta, 999, ";");
-
-//                $nombreDest[] = $leerDest[1];
-            echo $nombreDest[] = $leerDest[0] . "<->" . $leerDest[1] . "<br />";
-        }
-        //tenemos que mostrar en la web el nombre de las aerolineas. y los destinos de cada aerolinea en un radio boton 
-        //imprimo las aerolineas y sus destinos
-        for ($ind = 0; $ind < count($nombreAero); $ind++) {
-//                echo "<article>";
-//                echo "<h4>" . $nombreAero[$ind] . "</h4>";
-
-            for ($inde = 0; $inde < count($nombreDest); $inde++) {
-
-                if ($nombreAero[0] == $nombreDest[0]) {
-                    echo "-->" . $nombreDest[$inde];
-                }
-//                    echo"<input type='radio' name='destinos'>" . $nombreDest[$inde];
-//                    echo "</article>";
-            }
-        }
+        flock($f_id_nombreAerolinea_Dest, LOCK_UN);
+        fclose($f_id_nombreAerolinea_Dest);
         ?>
-        <form method="post" action="altaVuelos.php">
-            <br />
-            Seleccione Origen y pulse: <input type="submit" name="enviarDestino" value="Enviar">
-            
-        </form>
     </body>
 </html>
