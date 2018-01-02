8e0d736d5bd93127053544668d2428f547a86b7e
jalc <jantonio2511@gmail.com>
2018-01-02 17:58:00 +0100

    trabajo_pa

---
+++ HealthySocial/healthySocial/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HealthtyLife</title>
+        <meta name="viewport" content="width=device-width, user-scalabe=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Healthy Social</h1>
+        </header>
+        <nav>
+            <h3>Nav</h3>
+        </nav>
+
+        <div class="contendioPrincipal">
+            <section class="columna">
+                <h3>Secci&oacute;nLH</h3>
+                <article>
+                    <h4>ArticuloLH</h4>
+
+                </article>
+            </section>
+
+            <section class="columna">
+                <h3>Secci&oacute;RH</h3>
+                <article>
+                    <h4>ArticuloRH</h4>
+
+                </article>
+            </section>
+
+            <aside class="columna">
+                <h3>Aside</h3>
+            </aside>
+        </div>
+        <footer>
+            <h3>Footer</h3>
+        </footer>
+    </body>
+</html>
