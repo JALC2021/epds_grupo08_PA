935f936f7788df97e632a2ca8d5693d9354ac9f2
gdelrio87 <gdelrioala@gmail.com>
2018-01-21 03:56:54 +0100

    Corregido varios fallos

--- trabajo_red_social/HealthySocial/usuario/formularioContenido.php
+++ trabajo_red_social/HealthySocial/usuario/formularioContenido.php
@@ -464,7 +464,7 @@ if (isset($_SESSION['usuario'])) {
 
                                 <div class="row">
                                     <div class="col-25">
-                                        <label>D&oacute;sis</label>
+                                        <label>D&oacute;sis <span id="requerido"> (*)</span></label>
                                     </div>
                                     <div class="col-75">
                                         <input type="text" name="dosis" placeholder="En miligramos..."/>
